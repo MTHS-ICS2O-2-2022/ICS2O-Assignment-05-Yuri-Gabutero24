2bc72ba770334fb005de41a3834709f09c470008
Yuri-Gabutero24 <124618894+Yuri-Gabutero24@users.noreply.github.com>
2023-06-14 04:10:12 +0000

    all code

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Yuri G
+// Created on: May 2023
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function calculate() {
+  const term = parseInt(document.getElementById("multiply-term").value)
+
+  const sequence = []
+  for (let i=0; i<5; i++) {
+    sequence.push(term ** i);
+  }
+
+  document.getElementById("answer").innerHTML = "The first 5 terms would be: " + sequence
+}
